a5ed7f07d43c7d38a9777a04958b94421fd2ac6c
Xuan Wang <xwang@samaritan.ca>
2023-01-13 15:10:58 -0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+// import HomePage from './components/HomePage.vue';
+// import RobotBuilder from './build/RobotBuilder.vue';
+
 createApp(App).mount('#app');
